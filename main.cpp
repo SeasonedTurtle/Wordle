0826a5f7a65147f0244a9540e99c1d4e295c3430
Seasoned <85251573+SeasonedTurtle@users.noreply.github.com>
2022-02-24 14:24:19 -0500

    Updated and Refactored Main
    Implemented some changes to make it a bit neater, alongside adding functionality regarding the logic of the game wordle itself. There are still some bugs and things I need to add in the future.

--- main.cpp
+++ main.cpp
@@ -37,14 +37,32 @@ bool charCheck(std::string guess) {
 	return false;
 }
 
+// Avoids repeating chars by removing the repeated chars from the word then checking
+void charRepeat(std::string guess, std::vector<char> &guesses) {
+	for (int i = 0; i < guess.size(); i++) {
+		int count = 0;
+		for (int j = 0; j < guess.size(); j++) {
+			if (guess[i] == guess[j]) {
+				count++;
+			}
+			// remove repeated char from the string
+			if (guess[i] == guesses[j] && count >= 1) {
+				guess.erase(guess.begin() + j);
+			}
+		}
+	}
+	// append all of the chars in the string to the vector.
+	for (char &c : guess) {
+		guesses.push_back(c);
+	}
+}
+
 int wordListAppender(std::vector<std::string> &list) {
 	std::string word;
 	std::ifstream inFile;
 	inFile.open("wordleWordList.txt");
 	if (!inFile) {
-		std::cout << "File did not open."
-				  << "Program Terminated." 
-				  << "\n";
+		std::cout << "File did not open." << "\n\n";
 		return 1;
 	}
 	while (!inFile.eof()) {
@@ -89,6 +107,28 @@ int randomNumber() {
     
     return rng;
 }
+// Needs Work
+void vectorOutput(std::vector<char> list) {
+	std::cout << "\n";	
+	for (char &c : list) {
+		std::cout << c;
+	}
+	std::cout << "\n";
+}
+
+void endGame(bool &currentGameRunning, bool &running) {
+	std::string playAgain;
+	std::cout << "Do you want to play again? (yes/no)" << "\n";
+	std::cin >> playAgain;
+	lowerCase(playAgain);
+			
+	if (playAgain == "yes") {
+		currentGameRunning = false;
+	} else {
+		running = false;
+		currentGameRunning = false;
+	}
+}
 
 void wordCheck(std::string &guess) {
 	lowerCase(guess);
@@ -106,23 +146,27 @@ int main() {
 	
 	std::cout << "WORDLE: The Game" << "\n\n";
 	
+	// Main loop
 	while (running) {
 		std::vector<char> guesses;
+		std::vector<char> greenChars;
+		std::vector<char> yellowChars;
 		int guessCount = 0;
 		bool currentGameRunning = true;
 		bool wordGuessed = false;
-		char playAgain;
 		
 		int num = randomNumber();
 		std::string randomWord = wordList[num];
-
+		// Testing
 		std::cout << randomWord << "\n";
-
+		
 		gamesPlayed += 1;
 		
+		// sub loop
 		while (currentGameRunning) {
 			int count = 0;
 			std::string guess = "";
+			
 			std::cout << "Enter a five letter word." << "\n";
 			std::cin >> guess;
 			
@@ -134,34 +178,44 @@ int main() {
 				wordCheck(guess);
 			}
 			
+			charRepeat(guess, guesses);
+			
+			std::cout << "Chars Guessed: " << "\n";
+			vectorOutput(guesses);
+			std::cout << "\n";
+			
 			guessCount += 1;
 			
-			/*
-			for (char &c : guess) {
-				for (char &d: guesses) {
-					if (c != d) {
-						int count += 1;
-					} 
-					if (count == 0) {
-						guesses.push_back(c);
-					}
-				}
-			}
-			*/
+			/* 
+			If the guess is not correct it shows how many are in the right spot
+			and shows how many are in the word but not in the right spot
 			
-			std::cout << guess << "\n";
+			#1 - Currently there is a bug where a green letter thats is supposed to be 
+			     only green goes into the yellow list.
+			 
+			#2 - Can show multiple yellows for words that don't have that many letters. 
+			     Ex: Word ~ Sista and guess ~ sissy
+			     results in three yellows when there should be 2
+			*/
+			if (guess == randomWord) {
+				wordGuessed = true;
+			} else {
+				for (int i = 0; i < guess.size(); i++) {
+					for (int j = 0; j < randomWord.size(); j++) {
+						// If the chars are the same letter and index it's a green
+						if (guess[i] == randomWord[j] && i == j) {
+							greenChars.push_back(guess[i]);
+						} 
+						else if (guess[i] == randomWord[i] && i != j) {
+							yellowChars.push_back(guess[i]); 
+						}
+					}
+				}	
+			  }
+			
 			
 			if (guessCount >= 6 || wordGuessed) {
-				std::cout << "Do you want to play again? (y/n)" << "\n";
-				std::cin >> playAgain;
-				lowerCase(guess);
-				
-				if (playAgain == 'y') {
-					currentGameRunning = false;
-				} else {
-					running = false;
-					currentGameRunning = false;
-				}
+				endGame(currentGameRunning, running);
 			}
 		}
 	}
