5af52ff6a07bc70db48c1e7123bf8a2f3776b863
Seasoned <85251573+SeasonedTurtle@users.noreply.github.com>
2022-03-05 15:39:40 -0500

    new update
    Fixed segmentation fault, and the char repeat function which will have useful functionality for the actual game as well as the word bank.

--- main.cpp
+++ main.cpp
@@ -1,142 +1,4 @@
-#include <iostream>
-#include <fstream>
-#include <string>
-#include <vector>
-#include <algorithm>
-#include <random>
-#include <time.h>
-
-std::vector<std::string> wordList;
-
-bool validityCheck(std::string guess, std::vector<std::string> list) {
-	int count = 0;
-	for (std::string &w : list) {
-		if (guess == w) {
-			count++;
-		}
-	}
-	if (count > 0) {
-		return true;
-	}
-	return false;
-}
-
-bool charCheck(std::string guess) {
-	int count = 0;
-	std::string chars = "abcdefghijklmnopqrstuvwxyz";
-	for (char &c : guess) {
-		for (char &d : chars) { 
-			if (c == d) {
-				count += 1;
-			}
-		}
-	}
-	if (count >= 5) {
-		return true;
-	}
-	return false;
-}
-
-// Avoids repeating chars by removing the repeated chars from the word then checking
-void charRepeat(std::string guess, std::vector<char> &guesses) {
-	int size = guess.size();
-	for (int i = 0; i < size; i++) {
-		int count = 0;
-		for (int j = 0; j < size; j++) {
-			if (guess[i] == guess[j]) {
-				count++;
-			}
-			// remove repeated char from the string
-			if (guess[i] == guess[j] && count > 1) {
-				guess.erase(guess.begin() + j);
-			}
-		}
-	}
-	// append all of the chars in the string to the vector.
-	for (char &c : guess) {
-		guesses.push_back(c);
-	}
-}
-
-int wordListAppender(std::vector<std::string> &list) {
-	std::string word;
-	std::ifstream inFile;
-	inFile.open("wordleWordList.txt");
-	if (!inFile) {
-		std::cout << "File did not open." << "\n\n";
-		return 1;
-	}
-	while (!inFile.eof()) {
-		inFile >> word;
-		list.push_back(word);
-	}
-	inFile.close();
-
-	return 0;
-}
-
-void lowerCase(std::string &userInput) {
-    std::for_each(userInput.begin(), userInput.end(), [](char & c) {
-        c = ::tolower(c);
-    });
-}
-
-void charCheckPrompt(std::string &guess) {
-	while (!charCheck(guess)) {
-		std::cout << "Please enter valid characters." << "\n";
-		std::cin >> guess;
-		lowerCase(guess);
-	}
-}
-
-void lenCheck(std::string &guess) {
-	int size = guess.size();
-	while (size != 5) {
-		if (size < 5) {
-			std::cout << "Word is too short!" << "\n"; 
-		} else {
-			std::cout << "Word is too long!" << "\n"; 
-		}
-		std::cin >> guess;
-		lowerCase(guess);
-	}
-	charCheckPrompt(guess);
-}
-
-int randomNumber() {
-	srand (time(NULL));
-	int rng = rand() % 12972 + 1;
-    
-    return rng;
-}
-
-void vectorOutput(std::vector<char> list) {
-	std::cout << "\n";	
-	for (char &c : list) {
-		std::cout << c;
-	}
-	std::cout << "\n";
-}
-
-void endGame(bool &currentGameRunning, bool &running) {
-	std::string playAgain;
-	std::cout << "Do you want to play again? (yes/no)" << "\n";
-	std::cin >> playAgain;
-	lowerCase(playAgain);
-			
-	if (playAgain == "yes") {
-		currentGameRunning = false;
-	} else {
-		running = false;
-		currentGameRunning = false;
-	}
-}
-
-void wordCheck(std::string &guess) {
-	lowerCase(guess);
-	lenCheck(guess);
-	charCheckPrompt(guess);
-}
+#include "util.cpp"
 
 int main() {
 	wordListAppender(wordList);
@@ -159,15 +21,15 @@ int main() {
 		bool wordGuessed = false;
 		
 		int num = randomNumber();
-		std::string randomWord = wordList[num];
+		std::string randomWord = validWords[num];
+
 		// Testing
 		std::cout << randomWord << "\n";
 		
 		gamesPlayed += 1;
 		
-		// sub loop
+		// Sub loop
 		while (currentGameRunning) {
-			int count = 0;
 			std::string guess = "";
 			
 			std::cout << "Enter a five letter word." << "\n";
@@ -180,14 +42,10 @@ int main() {
 				std::cin >> guess;
 				wordCheck(guess);
 			}
-			
-			// Segmentation Fault Causer
-			charRepeat(guess, guesses);
-			
-			std::cout << "Words Guessed: " << "\n";
-			vectorOutput(guesses);
-			std::cout << "\n";
-			
+			// Word Bank
+			charRepeat(guess, guesses, guessCount);
+			std::cout << "\nLetters Guessed:\n";
+			output(guesses);
 			guessCount += 1;
 			
 			/* 
@@ -206,17 +64,24 @@ int main() {
 			} else {
 				for (int i = 0; i < guess.size(); i++) {
 					for (int j = 0; j < randomWord.size(); j++) {
-						// If the chars are the same letter and index it's a green
+						// If the chars are the same letter and the same location in the word it's a green
 						if (guess[i] == randomWord[j] && i == j) {
 							greenChars.push_back(guess[i]);
 						} 
 						else if (guess[i] == randomWord[i] && i != j) {
 							yellowChars.push_back(guess[i]); 
+							break;
 						}
 					}
 				}	
 			  }
-			
+			// Testing
+			std::cout << "\nGreen Letters:\n";
+			output(greenChars);
+
+			std::cout << "\nYellow Letters:\n";
+			output(yellowChars);
+
 			if (guessCount >= 6 || wordGuessed) {
 				endGame(currentGameRunning, running);
 			}
